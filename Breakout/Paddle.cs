46dd0b150a63a7c53875a947d9b361db3262a12d
Andreas Nyström <andy.i.flaskan@gmail.com>
2021-09-17 13:40:58 +0200

    ball created and can bounce, paddle created adn can be moved, collision class copied

---
+++ Breakout/Paddle.cs
@@ -0,0 +1,57 @@
+using System;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Breakout
+{
+    public class Paddle
+    {
+        public Sprite sprite;
+        public Vector2f size;
+
+        public Paddle()
+        {
+            sprite = new Sprite();
+            sprite.Texture = new Texture("assets/paddle.png");
+
+            Vector2f paddleTextureSize = (Vector2f) sprite.Texture.Size;
+            sprite.Origin = 0.5f * paddleTextureSize;
+            sprite.Scale = new Vector2f(paddleTextureSize.Y / paddleTextureSize.X ,paddleTextureSize.Y / paddleTextureSize.X);
+
+            sprite.Position = new Vector2f(Program.ScreenW * 0.5f, Program.ScreenH - 50);
+
+            size = new Vector2f(sprite.GetGlobalBounds().Width, sprite.GetGlobalBounds().Height);
+        }
+
+        public void Update(Ball ball, float deltaTime)
+        {
+            var newPos = sprite.Position;
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
+            {
+                newPos.X += deltaTime * 300f;
+            }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
+            {
+                newPos.X -= deltaTime * 300f; 
+            }
+            
+            if (newPos.X > Program.ScreenW - size.X/2)  //Why does dividing by 2 work? If you dont know just use 70px
+            {
+                newPos.X = Program.ScreenW - size.X/2;
+
+            }else if (newPos.X < 0 + size.X/2)
+            {
+                newPos.X = 0 + size.X/2;
+            }
+
+            sprite.Position= newPos;
+        }
+
+        public void Draw(RenderTarget target)
+        {
+            target.Draw(sprite);
+        }
+    }
+}
--- Breakout/Program.cs
+++ Breakout/Program.cs
@@ -7,20 +7,32 @@ namespace Breakout
 {
     class Program
     {
+        public const int ScreenW = 500;
+        public const int ScreenH = 700;
+
         static void Main(string[] args)
         {
-            using (var window = new RenderWindow(new VideoMode(500,700), "Breakout"))
+            using (var window = new RenderWindow(new VideoMode(ScreenW, ScreenH), "Breakout"))
             {
                 window.Closed += (o,e) => window.Close();
 
                 Clock clock = new Clock();
+                Ball ball = new Ball();
+                Paddle paddle = new Paddle();
+
                 while (window.IsOpen)
                 {
-                    float deltatime = clock.Restart().AsSeconds();
+                    float deltaTime = clock.Restart().AsSeconds();
 
                     window.DispatchEvents();
 
+                    ball.Update(deltaTime);
+                    paddle.Update(ball, deltaTime);
+
                     window.Clear(new Color(131, 197, 235));
+
+                    ball.Draw(window);
+                    paddle.Draw(window);
 
                     window.Display();
                 }
